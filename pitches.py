f0c896daaaa74ed06d0ceb5cd18d97275835d0a5
Greg Sabo <gregsabo@gmail.com>
2012-03-15 23:36:46 -0400

    added comment for pitches.py

--- pitches.py
+++ pitches.py
@@ -1,3 +1,14 @@
+"""
+pitches.py
+
+This file defines a set of convenience variables
+for hard-coding MIDI pitches. It uses lilypond-like
+pitch names, with '-is' indicating 'sharp' and '-es'
+indicating 'flat'.
+
+The pitches start at middle C but can easily be
+transposed by adding or subtracting multiples of twelve.
+"""
 c   = 60
 cis = 61
 des = 61
